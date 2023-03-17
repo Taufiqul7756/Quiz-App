a74930337f1f6a796fb9a219bbbfa98d73ea6502
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-03-17 15:54:19 +0600

    5 test questions added for quiz

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "Hyper Text Markup Language",
+    options: ["<scripting>", "<script>", "<JavaScript>", "<js>>"],
+  },
+  {
+    numb: 2,
+    question:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A And B",
+    options: ["Var", "let", "Both A And B", "None of the above"],
+  },
+  {
+    numb: 3,
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A And B",
+    options: [
+      "getElementbyId()",
+      "getElementbyClassName()",
+      "Both A And B",
+      "None of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "const",
+    options: ["const", "var", "let", "constant"],
+  },
+  //   {
+  //     numb: 5,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 6,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+  //   {
+  //     numb: 2,
+  //     question: "",
+  //     answer: "",
+  //     options: ["", "", "", ""],
+  //   },
+];
